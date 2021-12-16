54ed0ea36a661f9e8ae388e613f667c6c2e02700
chloemcbook <chloemcbook@chloes-mbp.lan>
2021-12-15 18:27:00 -0800

    ok

---
+++ src/_04_chuckle_clicker/ChuckleClicker.java
@@ -0,0 +1,24 @@
+package _04_chuckle_clicker;
+
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+
+public class ChuckleClicker {
+public static void main(String[] args) {
+	
+	makeButtons();
+	
+	
+	
+	
+	
+	
+	
+}
+
+private static void makeButtons() {
+	// TODO Auto-generated method stub
+	JOptionPane.showMessageDialog(null, "Make Buttons");
+	
+}
+}
